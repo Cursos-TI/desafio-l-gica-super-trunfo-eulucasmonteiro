4ffd9da139b95dece389e180777dafd480c5e2f5
Lucas Monteiro <lucasmonteiro180@gmail.com>
2026-06-05 22:47:34 +0000

    conclusao de nivel novato

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -16,16 +16,6 @@ int main() {
     float PIB_per_capita_1, PIB_per_capita_2;
     float inversodensidade_1, inversodensidade_2;
 
-    Densidade_Populacional_1 = (float) Populacao_1 / Area_1;
-    PIB_per_capita_1 = (PIB_1 * 1000000000.0) / Populacao_1;   
-    inversodensidade_1 = 1 / Densidade_Populacional_1;
-    float superpoder_1 = inversodensidade_1 + PIB_per_capita_1 + Pontos_Turisticos_1 + Area_1 + PIB_1 + Populacao_1;
-    
-    Densidade_Populacional_2 = (float) Populacao_2 / Area_2;        
-    PIB_per_capita_2 = (PIB_2 * 1000000000.0) / Populacao_2;    
-    inversodensidade_2 = 1 / Densidade_Populacional_2;
-    float superpoder_2 = inversodensidade_2 + PIB_per_capita_2 + Pontos_Turisticos_2 + Area_2 + PIB_2 + Populacao_2;
-
     
     // Cadastro das Cartas:
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
@@ -53,6 +43,11 @@ int main() {
     printf("Digite o numero de pontos turisticos: ");
     scanf("%d", &Pontos_Turisticos_1);
 
+    Densidade_Populacional_1 = (float) Populacao_1 / Area_1;
+    PIB_per_capita_1 = (PIB_1 * 1000000000.0) / Populacao_1;   
+    inversodensidade_1 = 1 / Densidade_Populacional_1;
+    float superpoder_1 = inversodensidade_1 + PIB_per_capita_1 + Pontos_Turisticos_1 + Area_1 + PIB_1 + Populacao_1;
+
     printf("\n\nCarta criada com sucesso!\n");
     printf("Insira os dados de sua segunda carta\n");
     
@@ -77,16 +72,20 @@ int main() {
     printf("Digite o numero de pontos turisticos: ");
     scanf("%d", &Pontos_Turisticos_2);
     
+    Densidade_Populacional_2 = (float) Populacao_2 / Area_2;        
+    PIB_per_capita_2 = (PIB_2 * 1000000000.0) / Populacao_2;    
+    inversodensidade_2 = 1 / Densidade_Populacional_2;
+    float superpoder_2 = inversodensidade_2 + PIB_per_capita_2 + Pontos_Turisticos_2 + Area_2 + PIB_2 + Populacao_2;
 
     // Comparação de Cartas:
     // Desenvolva a lógica de comparação entre duas cartas.
-    printf ("A carta 1 %s possui pib per capita de %.2f. \n", Cidade_1, PIB_per_capita_1);
-    printf ("A carta 2 %s possui pib per capita de %.2f. \n", Cidade_2, PIB_per_capita_2);
+    printf ("A carta 1 %s possui pib per capita de %.2f \n", Cidade_1, PIB_per_capita_1);
+    printf ("A carta 2 %s possui pib per capita de %.2f \n", Cidade_2, PIB_per_capita_2);
     if (PIB_per_capita_1 > PIB_per_capita_2){
-        printf ("A carta 1 venceu por ter maior pib per capita \n");
+        printf ("A carta 1 venceu por ter maior pib per capita!! \n");
     }
     else { 
-        printf ("A carta 2 venceu por ter maior pib per capita \n");
+        printf ("A carta 2 venceu por ter maior pib per capita!! \n");
     }
 
     return 0;
